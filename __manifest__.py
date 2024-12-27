f643c989fcd3302602694546f312c6d30c4435cf
pengdotevoke <otipapajim@gmail.com>
2024-12-27 12:15:55 +0300

    Some workaround for odoo community to work with automated stock

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Stock Accounting",
+    'version': "1.0",
+    'category': 'Inventory/Inventory',
+    'description': 'Stock valuation in community',
+    'depends': ['stock_account'],
+    'author':'Edward',
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': '',
+}
